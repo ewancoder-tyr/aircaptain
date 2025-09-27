1f7c2c6c6c48b5e2429829ad86599b8505b22426
Ivan Zyranau <ewancoder@gmail.com>
2025-09-28 00:37:31 +0400

    Temp testing (review)

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -171,9 +171,11 @@ else
 
     #if ! docker ps | grep -q "${project_container_name}_postgres"; then
     # A better way to check whether the stack is deployed.
-    if ! docker stack ls 2>/dev/null | grep -q "${project_container_name}"; then
+    if ! docker stack ls 2>/dev/null | grep -q "$project_container_name"; then
         # If the stack is not running - most likely it's the first deployment. Deploying the stack.
         docker stack ls # Temporarily
+        echo "project container is: ${project_container_name}"
+        echo "project container is: $project_container_name"
         echo "Database is not running, probably first time deployment. Deploying the stack and migrating DB."
         docker stack deploy "${stack_name}" --compose-file swarm-compose.yml --detach=false
 
